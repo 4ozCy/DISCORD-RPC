1c0e3b270ed58e8741ca4964857d147b2215bdb7
Wiston <157526994+Zyneeee@users.noreply.github.com>
2024-02-25 11:14:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,20 +21,19 @@ client.on('ready', async () => {
     .setApplicationId('1187238107209273345')
     .setType('PLAYING')
     .setURL('https://twitch.tv/discord')
-    .setState('Anxious - Best Bot')
 
-    .setName('.gg/CyberSec')
-    .setDetails('Discord Bot Developer, Web Developer')
+    .setName('Roblox')
+    .setDetails('Just Started')
     /*.setParty({
         max: 9999,
-        current: 6789,
+        current: 7000,
         id: Discord.getUUID(),
     })*/
 
     //.setStartTimestamp(Date.now())
-    .setAssetsLargeImage('https://media.discordapp.net/attachments/1058233402249187448/1092300908622250014/Picsart_23-03-13_04-24-35-528.jpg?width=634&height=634')
-    .setAssetsLargeText('CyberSec')
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1058233402249187448/1092301100796891206/030c530f07bdca30fc758eeacb22365a-modified.png');
+    .setAssetsLargeImage('https://media.discordapp.net/attachments/1187377061959045181/1211155528408104990/3a9ad6131057802cd6c6694d58cd7253.jpg?ex=65ed2b7c&is=65dab67c&hm=ac474442f75a5d961f6a91956fc5bdfa0ed757f81d15e706f8014242082b655b&')
+    .setAssetsLargeText('Roblox')
+  client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" });
 })
 
